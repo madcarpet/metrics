1ef85c603f40f951b9d04341347baca6602e2155
madcarpet <akvbox@proton.me>
2024-03-30 19:17:02 +0300

    fix import

--- internal/adapter/localdisk/dbimport.go
+++ internal/adapter/localdisk/dbimport.go
@@ -5,44 +5,49 @@ import (
 	"os"
 
 	"github.com/madcarpet/metrics/internal/entity"
+	"github.com/madcarpet/metrics/internal/logger"
 	"github.com/madcarpet/metrics/internal/service/metrics"
 )
 
 func DBImport(path string, us *metrics.UpdateMetricSvc) error {
 	if path != "" {
-		var metrics []entity.Metric
-		sf, err := os.OpenFile(path, os.O_RDONLY, 0655)
-		if err != nil {
-			return err
-		}
+		_, err := os.Stat(path)
+		if !os.IsNotExist(err) {
+			var metrics []entity.Metric
+			sf, err := os.OpenFile(path, os.O_RDONLY, 0655)
+			if err != nil {
+				return err
+			}
 
-		fileInfo, err := sf.Stat()
-		if err != nil {
-			return err
-		}
+			fileInfo, err := sf.Stat()
+			if err != nil {
+				return err
+			}
 
-		fData := make([]byte, fileInfo.Size())
+			fData := make([]byte, fileInfo.Size())
 
-		_, err = sf.Read(fData)
-		if err != nil {
-			return err
-		}
-		if err = sf.Close(); err != nil {
-			return err
-		}
-		err = json.Unmarshal(fData, &metrics)
-		if err != nil {
-			return err
-		}
-		if len(metrics) > 0 {
-			for _, m := range metrics {
-				err = us.UpdateMetric(m)
-				if err != nil {
-					return err
+			_, err = sf.Read(fData)
+			if err != nil {
+				return err
+			}
+			if err = sf.Close(); err != nil {
+				return err
+			}
+			err = json.Unmarshal(fData, &metrics)
+			if err != nil {
+				return err
+			}
+			if len(metrics) > 0 {
+				for _, m := range metrics {
+					err = us.UpdateMetric(m)
+					if err != nil {
+						return err
+					}
 				}
 			}
+		} else {
+			logger.Log.Info("DB File not found, import skipped")
 		}
-
 	}
 	return nil
 }
